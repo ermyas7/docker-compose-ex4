ee7d434e4e22c2ed23ff6b313343ea9e864c756b
ermyas7 <ermyas.fekadu@gmail.com>
2021-09-30 14:45:13 +0300

    Add error handling

--- service1.js
+++ service1.js
@@ -7,10 +7,16 @@ http.createServer((req, res) => {
     res.write(`To  ${req.client.localAddress}  : ${req.client.localPort}\n`);
 
     http.get(`http:localhost:${service2PORT}`, (response) => {
+
+        response.on('error', (chunk) => {
+            res.writeHead(404);
+            res.write('Service 2 is unreacheable!');
+            res.end();
+        })
+        
         response.on('data', (chunk) => {
             res.write(chunk);
         })
-
         response.on('end', () => {
             res.end();
           });
