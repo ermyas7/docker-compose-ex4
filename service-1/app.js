17d1016adc871cc87e23534e27fd442a264d634c
ermyas7 <ermyas.fekadu@gmail.com>
2021-10-02 01:23:40 +0300

    Add docker compose

--- service-1/app.js
+++ service-1/app.js
@@ -1,14 +1,12 @@
 const http = require('http');
 
-const service2PORT = 4000;
-
 http.createServer((req, res) => {
     res.write(`Hello from  ${req.client.remoteAddress} : ${req.client.remotePort}\n`);
     res.write(`To  ${req.client.localAddress}  : ${req.client.localPort}\n`);
-
-    http.get(`http:localhost:${service2PORT}`, (response) => {
-
-        response.on('error', (chunk) => {
+    const host = req.headers.host.split(':')[0];
+    http.get(`http://server2:4000`, (response) => {
+        response.on('error', (err) => {
+            console.log(err);
             res.writeHead(404);
             res.write('Service 2 is unreacheable!');
             res.end();
